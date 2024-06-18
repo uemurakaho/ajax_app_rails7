d8d79460b5355de5b213387942e124f42b43880f
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-06-18 14:27:38 +0900

    レスポンス処理修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -22,14 +22,14 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (XHR.status !=200) {
+      if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         // JavaScriptの処理から抜け出す、３０行以降の処理を行わない
         return null; 
       };
       const list = document.getElementById("list");
       const formText = document.getElementById("content");
-      list.insertAdjacentHTML("afterend", buildHTML(XMR));
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
       formText.value = "";
     };
   });
